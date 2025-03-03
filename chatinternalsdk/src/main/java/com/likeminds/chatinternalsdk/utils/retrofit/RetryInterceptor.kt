e6aa63703d62e3e7eee25fee8e080b5996d2fd2c
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2025-03-03 21:32:49 +0530

    resolved merge conflicts

--- chatinternalsdk/src/main/java/com/likeminds/chatinternalsdk/utils/retrofit/RetryInterceptor.kt
+++ chatinternalsdk/src/main/java/com/likeminds/chatinternalsdk/utils/retrofit/RetryInterceptor.kt
@@ -1,15 +1,12 @@
 package com.likeminds.chatinternalsdk.utils.retrofit
 
+import com.likeminds.chatinternalsdk.utils.retrofit.model.NetworkConstants
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 import kotlin.math.pow
 
 class RetryInterceptor @Inject constructor() : Interceptor {
-    companion object {
-        private const val MAX_RETRIES_ALLOWED = 3
-        private val RETRY_STATUS_CODES = setOf(500, 502, 503, 504, 408, 429)
-    }
 
     override fun intercept(chain: Interceptor.Chain): Response {
         return process(chain, 0)
@@ -20,12 +17,12 @@ class RetryInterceptor @Inject constructor() : Interceptor {
         try {
             val request = chain.request()
             response = chain.proceed(request)
-            if (attempt < MAX_RETRIES_ALLOWED && response.code in RETRY_STATUS_CODES) {
+            if (attempt < NetworkConstants.MAX_RETRIES_ALLOWED && response.code in NetworkConstants.retryErrorCodes) {
                 return delayedAttempt(chain, response, attempt)
             }
             return response
         } catch (e: Exception) {
-            if (attempt < MAX_RETRIES_ALLOWED) {
+            if (attempt < NetworkConstants.MAX_RETRIES_ALLOWED) {
                 return delayedAttempt(chain, response, attempt)
             }
             throw e
--- chatinternalsdk/src/main/java/com/likeminds/chatinternalsdk/utils/retrofit/model/NetworkConstants.kt
+++ chatinternalsdk/src/main/java/com/likeminds/chatinternalsdk/utils/retrofit/model/NetworkConstants.kt
@@ -1,13 +1,23 @@
 package com.likeminds.chatinternalsdk.utils.retrofit.model
 
 object NetworkConstants {
+    const val MAX_RETRIES_ALLOWED = 3
+
     // error codes
     const val UNAUTHORIZED = 401
-    const val SERVER_ERROR = 500
-    const val BAD_GATEWAY = 502
-    const val SERVICE_UNAVAILABLE = 503
-    const val GATEWAY_TIMEOUT = 504
-    const val TOO_MANY_REQUESTS = 429
+    private const val SERVER_ERROR = 500
+    private const val BAD_GATEWAY = 502
+    private const val SERVICE_UNAVAILABLE = 503
+    private const val GATEWAY_TIMEOUT = 504
+    private const val TOO_MANY_REQUESTS = 429
+
+    val retryErrorCodes = setOf(
+        SERVER_ERROR,
+        BAD_GATEWAY,
+        SERVICE_UNAVAILABLE,
+        GATEWAY_TIMEOUT,
+        TOO_MANY_REQUESTS
+    )
 
     //headers
     const val X_PLATFORM_CODE = "x-platform-code"
